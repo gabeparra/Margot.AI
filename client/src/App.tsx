9428303aebe84d8d23b907ef67cd9b6459531136
Pharit Smitasin <pharitsmitasin@gmail.com>
2023-10-07 02:40:33 -0400

    remove old comments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,14 +20,12 @@ function App() {
         </a>
         <button className="change-language">SITE LANGUAGE: English</button>
       </div>
-      {/* <div className="images"> */}
       <img src={starImage} className="star-image" alt="Image of a star" />
       <img
         src={girlPointing}
         className="girlPointing-image"
         alt="girl pointing at text"
       />
-      {/* </div> */}
       <div className="instructions">
         <p className="instructions-english-line1">
           Helping your kids learn and
