aee09109a443c235d59f1dad31ccaa6f6f631ccf
Pharit Smitasin <pharitsmitasin@gmail.com>
2023-10-06 23:21:03 -0400

    vite initialization w/ react

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import babyImage from "./assets/baby.png";
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="header">
+        <h1 className="title">Margot.AI</h1>
+        <a>
+          <img
+            src={babyImage}
+            className="baby-image"
+            alt="Image of a baby's face"
+          />
+        </a>
+        <button className="change-language">SITE LANGUAGE: English</button>
+      </div>
+      <div>
+        {/* <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a> */}
+        {/* <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a> */}
+      </div>
+      {/* <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+    </>
+  );
+}
+
+export default App;
