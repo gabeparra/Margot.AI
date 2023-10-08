2fcc60fcafa5900432d99dceda14febd9c33284f
gabeparra <ma.parrar16@gmail.com>
2023-10-08 06:56:20 -0400

    more tries

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    proxy: {
+      '/api': 'http://localhost:8080'
+    },
     port: 8080
   },
   preview: {
